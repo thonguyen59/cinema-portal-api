bf328fe9d2a9c5a0dfec9d105d48268c9fe6d4f4
ThoNguyen <thonguyen0110.work@gmail.com>
2023-11-11 19:27:14 +0700

    add source

---
+++ src/main/java/com/uit/cinemaportalapi/CinemaPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.uit.cinemaportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaPortalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaPortalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uit/cinemaportalapi/CinemaPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uit.cinemaportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaPortalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
